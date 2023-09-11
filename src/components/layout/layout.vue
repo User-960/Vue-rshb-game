7e6db773d95efa8dcda0d7863780019f3d2170d1
Ivan <ivanvolodin960@gmail.com>
2023-09-11 19:26:17 +0300

    feat: add component layout

---
+++ src/components/layout/layout.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class='styles.mainWrapper'>
+    <header>
+      <slot name="header"></slot>
+    </header>
+
+    <main :class='styles.contentWrapper'>
+      <slot></slot>
+    </main>
+
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'layout',
+})
+</script>
+
+<style src="./layout.css" module='styles'>
+
+</style>
--- src/pages/mainPage.vue
+++ src/pages/mainPage.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="home">
-    <h1>Main Page</h1>
-  </div>
+  <layout>
+    <template v-slot:default>
+      <p>Параграф для основного контента.</p>
+    </template>
+  </layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import layout from '@/components/layout/layout.vue'
 
 export default Vue.extend({
   name: 'mainPage',
-  components: {},
+  components: {
+    layout
+  },
 });
 </script>
