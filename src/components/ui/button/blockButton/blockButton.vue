970fa950a89ff638005d2535368f8cabab7b3236
Ivan <ivanvolodin960@gmail.com>
2023-09-13 02:19:49 +0300

    feat: add ui component blockButton

---
+++ src/components/ui/button/blockButton/blockButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class='styles.blockBtn'>
+    <slot name="contentIconBtn"></slot>
+    
+    <div :class='styles.indicator'>
+      <slot name="contentIndicator"></slot>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'blockButton',
+  methods: {}
+})
+</script>
+
+<style src='./blockButton.css' module='styles'>
+  
+</style>
