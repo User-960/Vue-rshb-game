e20e18854a0cddeff965d27d4fdb76bbfc2ab424
Ivan <ivanvolodin960@gmail.com>
2023-09-15 01:03:36 +0300

    fix: fix mapBack

--- src/components/screens/home/mapBack/mapBack.vue
+++ src/components/screens/home/mapBack/mapBack.vue
@@ -1,6 +1,8 @@
 <template>
   <div :class='styles.map'>
-    
+    <div :class='styles.lab'></div>
+    <div :class='[styles.house, {available: isHouseGameAvailable}]'></div>
+    <div :class='[styles.greenhouse, {available: isGreenHouseGameAvailable}]'></div>
   </div>
 </template>
 
@@ -8,7 +10,11 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'mapBack'
+  name: 'mapBack',
+  data: () => ({
+    isHouseGameAvailable: false,
+    isGreenHouseGameAvailable: false,
+  })
 })
 </script>
 
