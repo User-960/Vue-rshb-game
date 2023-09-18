180388ad7b0068d170cedfb54a9f276dd1a09b95
Ivan <ivanvolodin960@gmail.com>
2023-09-19 02:34:36 +0300

    refactor: add closeButton

--- src/components/screens/home/modalBank/modalBank.vue
+++ src/components/screens/home/modalBank/modalBank.vue
@@ -1,16 +1,7 @@
 <template>
   <div :class='styles.modalBankWrapper' v-if='GET_MODAL_BANK_VISIBLE'>
     <div :class='styles.modalBank' data-testid='modalBank'>
-      <button 
-        @click="$store.commit('HIDE_MODAL_BANK')" 
-        :class='styles.close' 
-        aria-label='close modal window' 
-        data-testid='closeModalBtn'
-      >
-        <svg width="30" height="30" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M4.11 2.697L2.698 4.11 6.586 8l-3.89 3.89 1.415 1.413L8 9.414l3.89 3.89 1.413-1.415L9.414 8l3.89-3.89-1.415-1.413L8 6.586l-3.89-3.89z" fill="#e60c0cc8"></path>
-        </svg>
-      </button>
+      <closeButton @onclick="$store.commit('HIDE_MODAL_BANK')"/>
 
       <bank v-if='isFirstCredit'>
         <template v-slot:contentImg>
@@ -104,6 +95,7 @@
 <script lang='ts'>
 import Vue from 'vue'
 import linkButton from '../../../ui/button/linkButton/linkButton.vue'
+import closeButton from '../../../ui/button/closeButton/closeButton.vue'
 import { mapMutations, mapGetters } from 'vuex'
 import { EMapBackGetters } from '../../../../store/modules/homeScreen/getters'
 import { EHomeScreenMutation } from '../../../../store/modules/homeScreen/mutations'
@@ -120,7 +112,8 @@ export default Vue.extend({
   components: {
     skipButton,
     linkButton,
-    bank
+    bank,
+    closeButton
   },
   computed: {
     ...mapGetters([EMapBackGetters.GET_MODAL_BANK_VISIBLE]),
