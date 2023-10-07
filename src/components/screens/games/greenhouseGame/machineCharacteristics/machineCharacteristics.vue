e2be8b416bb47f2870315715b0e13f953fcaa105
Ivan <ivanvolodin960@gmail.com>
2023-10-07 16:01:48 +0300

    feat: add module in Vuex for greenhouseGame

--- src/components/screens/games/greenhouseGame/machineCharacteristics/machineCharacteristics.vue
+++ src/components/screens/games/greenhouseGame/machineCharacteristics/machineCharacteristics.vue
@@ -1,40 +1,40 @@
 <template>
   <div :class='styles.machineCharacteristicsBack'>
-    <div v-if='GET_TOMATO_LEVEL' :class='[
+    <div v-if='GET_TOMATO_LEVEL_GH' :class='[
         styles.plant, 
         {
-          [styles.plantSproutTomato]: GET_TOMATO_SPROUT,
-          [styles.plantColorTomato]: GET_TOMATO_COLOR,
-          [styles.plantModifiedTomato]: GET_TOMATO_MODIFIED
+          [styles.plantSproutTomato]: GET_TOMATO_SPROUT_GH,
+          [styles.plantColorTomato]: GET_TOMATO_COLOR_GH,
+          [styles.plantModifiedTomato]: GET_TOMATO_MODIFIED_GH
         }
       ]'
     ></div>
 
-    <div v-else-if='GET_PEPPER_LEVEL' :class='[
+    <div v-else-if='GET_PEPPER_LEVEL_GH' :class='[
         styles.plant, 
         {
-          [styles.plantSproutPepper]: GET_PEPPER_SPROUT,
-          [styles.plantColorPepper]: GET_PEPPER_COLOR,
-          [styles.plantModifiedPepper]: GET_PEPPER_MODIFIED
+          [styles.plantSproutPepper]: GET_PEPPER_SPROUT_GH,
+          [styles.plantColorPepper]: GET_PEPPER_COLOR_GH,
+          [styles.plantModifiedPepper]: GET_PEPPER_MODIFIED_GH
         }
       ]'
     ></div>
 
-    <div v-else-if='GET_STRAWBERRY_LEVEL' :class='[
+    <div v-else-if='GET_STRAWBERRY_LEVEL_GH' :class='[
         styles.plant, 
         {
-          [styles.plantSproutStrawberry]: GET_STRAWBERRY_SPROUT,
-          [styles.plantColorStrawberry]: GET_STRAWBERRY_COLOR,
-          [styles.plantModifiedStrawberry]: GET_STRAWBERRY_MODIFIED
+          [styles.plantSproutStrawberry]: GET_STRAWBERRY_SPROUT_GH,
+          [styles.plantColorStrawberry]: GET_STRAWBERRY_COLOR_GH,
+          [styles.plantModifiedStrawberry]: GET_STRAWBERRY_MODIFIED_GH
         }
       ]'
     ></div>
 
     <div :class='styles.bigFlaskEmpty'></div>
 
-    <flasksTomato v-if='GET_TOMATO_LEVEL' />
-    <flasksPepper v-else-if='GET_PEPPER_LEVEL' />
-    <flasksStrawberry v-else-if='GET_STRAWBERRY_LEVEL' />
+    <!-- <flasksTomato v-if='GET_TOMATO_LEVEL_GH' />
+    <flasksPepper v-else-if='GET_PEPPER_LEVEL_GH' />
+    <flasksStrawberry v-else-if='GET_STRAWBERRY_LEVEL_GH' /> -->
   </div>
 </template>
 
@@ -44,8 +44,8 @@ import flasksTomato from './flasks/flasksTomato.vue'
 import flasksPepper from './flasks/flasksPepper.vue'
 import flasksStrawberry from './flasks/flasksStrawberry.vue'
 import { mapGetters, mapMutations } from 'vuex'
-import { EN_GeneticGameGetters } from '@/store/modules/geneticGame/getters'
-import { EN_GeneticGameMutation } from '@/store/modules/geneticGame/mutations'
+import { EN_GreenhouseGameGetters } from '@/store/modules/greenhouseGame/getters'
+import { EN_GreenhouseGameMutation } from '@/store/modules/greenhouseGame/mutations'
 
 export default Vue.extend({
   name: 'machineCharacteristics',
@@ -56,30 +56,30 @@ export default Vue.extend({
   },
   computed: {
     ...mapGetters([
-      EN_GeneticGameGetters.GET_TOMATO_LEVEL,
-      EN_GeneticGameGetters.GET_TOMATO_SPROUT,
-      EN_GeneticGameGetters.GET_TOMATO_COLOR,
-      EN_GeneticGameGetters.GET_TOMATO_MODIFIED,
+      EN_GreenhouseGameGetters.GET_TOMATO_LEVEL_GH,
+      EN_GreenhouseGameGetters.GET_TOMATO_SPROUT_GH,
+      EN_GreenhouseGameGetters.GET_TOMATO_COLOR_GH,
+      EN_GreenhouseGameGetters.GET_TOMATO_MODIFIED_GH,
 
-      EN_GeneticGameGetters.GET_PEPPER_LEVEL,
-      EN_GeneticGameGetters.GET_PEPPER_SPROUT,
-      EN_GeneticGameGetters.GET_PEPPER_COLOR,
-      EN_GeneticGameGetters.GET_PEPPER_MODIFIED,
+      EN_GreenhouseGameGetters.GET_PEPPER_LEVEL_GH,
+      EN_GreenhouseGameGetters.GET_PEPPER_SPROUT_GH,
+      EN_GreenhouseGameGetters.GET_PEPPER_COLOR_GH,
+      EN_GreenhouseGameGetters.GET_PEPPER_MODIFIED_GH,
 
-      EN_GeneticGameGetters.GET_STRAWBERRY_LEVEL,
-      EN_GeneticGameGetters.GET_STRAWBERRY_SPROUT,
-      EN_GeneticGameGetters.GET_STRAWBERRY_COLOR,
-      EN_GeneticGameGetters.GET_STRAWBERRY_MODIFIED,
+      EN_GreenhouseGameGetters.GET_STRAWBERRY_LEVEL_GH,
+      EN_GreenhouseGameGetters.GET_STRAWBERRY_SPROUT_GH,
+      EN_GreenhouseGameGetters.GET_STRAWBERRY_COLOR_GH,
+      EN_GreenhouseGameGetters.GET_STRAWBERRY_MODIFIED_GH,
     ]),
   },
   methods: {
     ...mapMutations([
-      EN_GeneticGameMutation.START_PEPPER_LEVEL,
-      EN_GeneticGameMutation.START_STRAWBERRY_LEVEL,
-      EN_GeneticGameMutation.FINISH_STRAWBERRY_LEVEL,
-      EN_GeneticGameMutation.HIDE_TOMATO_MODIFIED,
-      EN_GeneticGameMutation.HIDE_PEPPER_MODIFIED,
-      EN_GeneticGameMutation.HIDE_STRAWBERRY_MODIFIED,
+      EN_GreenhouseGameMutation.START_PEPPER_LEVEL_GH,
+      EN_GreenhouseGameMutation.START_STRAWBERRY_LEVEL_GH,
+      EN_GreenhouseGameMutation.FINISH_STRAWBERRY_LEVEL_GH,
+      EN_GreenhouseGameMutation.HIDE_TOMATO_MODIFIED_GH,
+      EN_GreenhouseGameMutation.HIDE_PEPPER_MODIFIED_GH,
+      EN_GreenhouseGameMutation.HIDE_STRAWBERRY_MODIFIED_GH,
     ]),
   }
 })
