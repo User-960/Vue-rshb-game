e666b7dc80e138eb9b345ab77fd7e8c2c2e20470
Ivan <ivanvolodin960@gmail.com>
2023-10-11 02:43:49 +0300

    feat: add tomato and gravity in collectorGame

--- src/components/screens/games/collectorGame/collectorGameField/collectorGameFieldTomato.vue
+++ src/components/screens/games/collectorGame/collectorGameField/collectorGameFieldTomato.vue
@@ -9,7 +9,11 @@
 
           <div :class='styles.cellWrapper'>
             <div v-for='cell in cells' :key='cell.id'>
-              <div :class='[styles.cell, {[styles.isTomatoGreen]: cell.isTomatoGreen}]'></div>
+              <div :class='[styles.cell]'>
+                <div :class='[{[styles.isTomatoGreen]: cell.isTomatoGreen}]'></div>
+                <div :class='[{[styles.isTomatoRed]: cell.isTomatoRed}]'></div>
+                <div :class='[{[styles.isTomatoDarkGreen]: cell.isTomatoDarkGreen}]'></div>
+              </div>
             </div>
           </div>
 
@@ -24,21 +28,67 @@
 import { EN_CollectorGameGetters } from '@/store/modules/collectorGame/getters'
 import Vue from 'vue'
 import { mapGetters } from 'vuex'
-import { generatorCells } from '../helpers/helpers'
+import { generatorCells, generatorTomatoGreen, generatorTomatoRed, generatorTomatoDarkGreen } from '../helpers/helpers'
 
 const cells = generatorCells()
+const firstColumn: number[] = [1, 14, 27, 40, 53, 66, 79, 92, 105, 118]
+const secondColumn: number[] = [2, 15, 28, 41, 54, 67, 80, 93, 106, 119]
+const fifthColumn: number[] = [5, 18, 31, 44, 57, 70, 83, 96, 109, 122]
+const ninthColumn: number[] = [9, 22, 35, 48, 61, 74, 87, 100, 113, 126]
 
 export default Vue.extend({
   name: 'collectorGameFieldTomato',
   data: () => ({
     isBlackScreenShow: false,
     cells,
+    generateTomatoGreen: false,
+    generateTomatoRed: false,
+    generateTomatoDarkGreen: false
   }),
   components: {},
   created() {
     setTimeout(() => {
       this.isBlackScreenShow = true
+      this.generateTomatoGreen = true
     }, 2350)
+
+    setTimeout(() => {
+      this.generateTomatoGreen = true
+    }, 2500)
+
+    setTimeout(() => {
+      this.generateTomatoRed = true
+    }, 3000)
+
+    setTimeout(() => {
+      this.generateTomatoDarkGreen = true
+    }, 4000)
+  },
+  watch: {
+    generateTomatoGreen() {
+      if (this.generateTomatoGreen) {
+        const greenTomato = generatorTomatoGreen(cells, fifthColumn, 5, 0)
+        if (!greenTomato) {
+          this.generateTomatoGreen = false
+        }
+      }
+    },
+    generateTomatoRed() {
+      if (this.generateTomatoRed) {
+        const redTomato = generatorTomatoRed(cells, secondColumn, 2, 0)
+        if (!redTomato) {
+          this.generateTomatoRed = false
+        }
+      }
+    },
+    generateTomatoDarkGreen() {
+      if (this.generateTomatoDarkGreen) {
+        const darkGreenTomato = generatorTomatoDarkGreen(cells, ninthColumn, 9, 0)
+        if (!darkGreenTomato) {
+          this.generateTomatoDarkGreen = false
+        }
+      }
+    }
   },
   computed: {
     ...mapGetters([EN_CollectorGameGetters.GET_OPEN_GAME_FIELD_TOMATO_COL]),
--- src/components/screens/games/collectorGame/helpers/helpers.ts
+++ src/components/screens/games/collectorGame/helpers/helpers.ts
@@ -9,14 +9,135 @@ const cells = [
 
 export const generatorCells = () => {
 	let newCells = []
+	let randomNum = 4
 	for (let i = 0; i < 130; i++) {
 		newCells.push({
 			id: i,
 			isTomatoRed: false,
-			isTomatoGreen: true,
+			isTomatoGreen: false,
 			isTomatoDarkGreen: false
 		})
 	}
 
 	return newCells
+}
+
+interface ICell {
+	id: number
+	isTomatoRed: boolean
+	isTomatoGreen: boolean
+	isTomatoDarkGreen: boolean
+}
+
+const fifthColumn: number[] = [5, 18, 31, 44, 57, 70, 83, 96, 109, 122]
+
+// export const generatorTomatoGreen = (cells: ICell[], column: number[]) => {
+// 	let testTomato: ICell[]
+// 	testTomato = cells.filter(cell => cell.id === column[0])
+
+// 	if (testTomato[0].id === 5) {
+// 		testTomato[0].isTomatoGreen = true
+// 		setTimeout(() => {
+// 			testTomato[0].isTomatoGreen = false
+// 			testTomato = cells.filter(cell => cell.id === column[1])
+
+// 			if (testTomato[0].id === 18) {
+// 				testTomato[0].isTomatoGreen = true
+
+// 				setTimeout(() => {
+// 					testTomato[0].isTomatoGreen = false
+// 					testTomato = cells.filter(cell => cell.id === column[2])
+
+// 					if (testTomato[0].id === 31) {
+// 						testTomato[0].isTomatoGreen = true
+
+// 						setTimeout(() => {
+// 							testTomato[0].isTomatoGreen = false
+// 						}, 1000)
+// 					}
+// 				}, 1000)
+// 			}
+// 		}, 1000)
+// 	}
+// }
+
+export const generatorTomatoGreen = (
+	cells: ICell[],
+	column: number[],
+	columnCellId: number = 5,
+	columnCellIndex: number = 0
+) => {
+	let testTomato: ICell[]
+
+	testTomato = cells.filter(cell => cell.id === column[columnCellIndex])
+
+	if (testTomato[0].id === column[column.length - 1]) {
+		return false
+	}
+
+	if (testTomato[0].id === columnCellId) {
+		testTomato[0].isTomatoGreen = true
+
+		setTimeout(() => {
+			testTomato[0].isTomatoGreen = false
+
+			columnCellId += 13
+			columnCellIndex += 1
+			generatorTomatoGreen(cells, column, columnCellId, columnCellIndex)
+		}, 700)
+	}
+}
+
+export const generatorTomatoRed = (
+	cells: ICell[],
+	column: number[],
+	columnCellId: number = 2,
+	columnCellIndex: number = 0
+) => {
+	let testTomato: ICell[]
+
+	testTomato = cells.filter(cell => cell.id === column[columnCellIndex])
+
+	if (testTomato[0].id === column[column.length - 1]) {
+		return false
+	}
+
+	if (testTomato[0].id === columnCellId) {
+		testTomato[0].isTomatoRed = true
+
+		setTimeout(() => {
+			testTomato[0].isTomatoRed = false
+
+			columnCellId += 13
+			columnCellIndex += 1
+			generatorTomatoRed(cells, column, columnCellId, columnCellIndex)
+		}, 700)
+	}
+}
+
+export const generatorTomatoDarkGreen = (
+	cells: ICell[],
+	column: number[],
+	columnCellId: number,
+	columnCellIndex: number
+) => {
+	let testTomato: ICell[]
+
+	testTomato = cells.filter(cell => cell.id === column[columnCellIndex])
+
+	if (testTomato[0].id === column[column.length - 1]) {
+		return false
+	}
+
+	if (testTomato[0].id === columnCellId) {
+		testTomato[0].isTomatoDarkGreen = true
+
+		setTimeout(() => {
+			testTomato[0].isTomatoDarkGreen = false
+
+			columnCellId += 13
+			columnCellIndex += 1
+			generatorTomatoDarkGreen(cells, column, columnCellId, columnCellIndex)
+		}, 700)
+	}
 }
