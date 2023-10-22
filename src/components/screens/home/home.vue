a5c21dd925d14e754ac97b3196f6c5610f01854e
Ivan <ivanvolodin960@gmail.com>
2023-10-22 20:01:25 +0300

    fix: add orintation for homaePage

--- src/components/screens/home/home.vue
+++ src/components/screens/home/home.vue
@@ -51,7 +51,7 @@ export default Vue.extend({
   created() {
     this.PLAY_BACK_MUSIC_MAP()
 
-    if (window.innerWidth < 960) {
+    if (window.innerWidth < 800) {
       document.body.style.transform = 'rotate(90deg)'
     }
   },
