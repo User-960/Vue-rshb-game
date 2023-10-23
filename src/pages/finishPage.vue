70c5ebf2caebaa71c197c2c8784bba969426d033
Ivan <ivanvolodin960@gmail.com>
2023-10-23 13:24:02 +0300

    feat: add close button fot table rating

--- src/pages/finishPage.vue
+++ src/pages/finishPage.vue
@@ -1,8 +1,6 @@
 <template>
-  <!-- <finish v-if='GET_PLAYER_DATA.credit === 0 && GET_PLAYER_DATA.minigame.gameFive.complete' />
-  <alert v-else /> -->
-
-  <finish />
+  <finish v-if='GET_PLAYER_DATA.credit === 0 && GET_PLAYER_DATA.minigame.gameFive.complete' />
+  <alert v-else />
 </template>
 
 <script lang="ts">
@@ -23,23 +21,23 @@ export default Vue.extend({
   created() {
     const playerData = localStorage.getItem(EN_USER.PLAYER_DATA)
 
-    // if (playerData) {
-    //   this.GET_PLAYER(JSON.parse(playerData).id)
-    // } else {
-    //   this.$router.push({ name: 'start' })
-    // }
+    if (playerData) {
+      this.GET_PLAYER(JSON.parse(playerData).id)
+    } else {
+      this.$router.push({ name: 'start' })
+    }
 
-    // if (playerData) {
-    //   if (
-    //       JSON.parse(playerData).minigame.gameOne.complete !== true && 
-    //       JSON.parse(playerData).minigame.gameTwo.complete !== true && 
-    //       JSON.parse(playerData).minigame.gameThree.complete !== true && 
-    //       JSON.parse(playerData).minigame.gameFour.complete !== true && 
-    //       JSON.parse(playerData).minigame.gameFive.complete !== true
-    //      ) {
-    //     this.$router.push({ name: 'home' })
-    //   }
-    // }
+    if (playerData) {
+      if (
+          JSON.parse(playerData).minigame.gameOne.complete !== true && 
+          JSON.parse(playerData).minigame.gameTwo.complete !== true && 
+          JSON.parse(playerData).minigame.gameThree.complete !== true && 
+          JSON.parse(playerData).minigame.gameFour.complete !== true && 
+          JSON.parse(playerData).minigame.gameFive.complete !== true
+         ) {
+        this.$router.push({ name: 'home' })
+      }
+    }
   },
   computed: {
     ...mapGetters([
