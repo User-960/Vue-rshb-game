01436ff4f34b8e642632995de0af50b7ec184887
Ivan <ivanvolodin960@gmail.com>
2023-10-30 14:49:24 +0300

    feat: add social links

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import VueCompositionAPI from '@vue/composition-api'
 import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
 
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/assets/styles/index.css'
+
+Vue.use(VueClipboard)
 
 Vue.config.productionTip = false
 Vue.use(VueCompositionAPI)
